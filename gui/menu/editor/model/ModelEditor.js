f92b1c4c27a3255034040d50fec26055aafa2698
BuildTools <unconfigured@null.spigotmc.org>
2018-10-26 13:40:44 +0200

    Work on model editor

--- gui/menu/editor/model/ModelEditor.js
+++ gui/menu/editor/model/ModelEditor.js
@@ -1,20 +1,49 @@
-function GuiModelEditor(name, saveMode, modelBuilder){
+function GuiModelEditor(name, modelBuilder, save, saveAs, exit){
 	this.didInit = false;
 	this.components = [];
 
 	this.name = name;
-	this.saveMode = saveMode;
 	this.builder = modelBuilder;
+	this.save = save;
+	this.saveAs = saveAs;
+	this.exit = exit;
+
+	this.hasChanges = false;
 }
 
 extendProtoType(Gui.Menu, GuiModelEditor);
 
 GuiModelEditor.prototype.backgroundColor = 'rgb(0, 50, 250)';
 
-GuiModelEditor.prototype.addComponents = function(){
-	this.tabProperties = TextProperties.tab('rgb(200,200,250)', 'rgb(150,150,250)');
-	this.tabHoverProperties = TextProperties.hoverTab('rgb(170,170,250)', 'rgb(120,120,250)');
-	this.tabActiveProperties = TextProperties.tab('rgb(130,130,250)', 'rgb(100,100,250)');
+GuiModelEditor.prototype.tabProperties = TextProperties.tab('rgb(200,200,250)', 'rgb(150,150,250)');
+GuiModelEditor.prototype.tabHoverProperties = TextProperties.hoverTab('rgb(170,170,250)', 'rgb(120,120,250)');
+GuiModelEditor.prototype.tabActiveProperties = TextProperties.tab('rgb(130,130,250)', 'rgb(100,100,250)');
 
-	this.addFullComponent(new Gui.Toolbar());//TODO create toolbars...
+GuiModelEditor.prototype.addComponents = function(){
+	this.addComponent(new Gui.BackgroundComponent(this.backgroundColor), 0.0, 0.9, 1.0, 1.0);
+	this.addFullComponent(ModelEditorToolbars.createFile(this));
+};
+
+GuiModelEditor.prototype.setPopup = function(popup){
+	if(popup){
+		this.state.getManager().setMainComponent(new Gui.PopupMenu(this, popup, 0.25, 0.25, 0.75, 0.75));
+	} else {
+		this.state.getManager().setMainComponent(this);
+	}
+};
+
+const ModelEditorToolbars = {
+	upperProps : TextProperties.label('black', 'rgb(200,200,250)'),
+	upperHoverProps : TextProperties.label('black', 'rgb(170,170,250)'),
+	upperActiveProps : TextProperties.label('black', 'rgb(130,130,250)'),
+
+	props : TextProperties.listElement('rgb(180,180,250)', 'rgb(40,40,60)'),
+	hoverProps : TextProperties.listElement('rgb(120,120,250)', 'rgb(30,30,40)')
+};
+
+const ModelEditorPopups = {
+	labelProps : TextProperties.label('rgb(200,200,200)'),
+	buttonProps : TextProperties.button('rgb(100,0,200)', 'rgb(200,0,255)'),
+	buttonHoverProps : TextProperties.hoverButton('rgb(130,0,255)', 'rgb(255,0,255)'),
+	backgroundColor : 'rgb(25, 0, 100)'
 };
---
+++ gui/menu/editor/model/toolbar/File.js
@@ -0,0 +1,32 @@
+ModelEditorToolbars.createFile = function(editor){
+	const t = ModelEditorToolbars;
+	const toolbar = new Gui.Toolbar('File', t.upperProps, t.upperHoverProps, t.upperActiveProps, 0.05, 0.05, 0.25, 0.15, function(){
+		this.addComponent(new Gui.TextComponent('Save', t.props, t.hoverProps, function(x, y, button){
+			editor.save();
+		}), 0.05);
+		this.addComponent(new Gui.TextComponent('Save as', t.props, t.hoverProps, function(x, y, button){
+			editor.saveAs();
+		}), 0.05);
+		this.addComponent(new Gui.TextComponent('Exit', t.props, t.hoverProps, function(x, y, button){
+			if (editor.hasChanges){
+				const popup = new Gui.Menu(ModelEditorPopups.backgroundColor, function(){
+					this.addComponent(new Gui.TextComponent('Would you like to save?', ModelEditorPopups.label), 0.2, 0.6, 0.8, 0.8);
+					this.addComponent(new Gui.TextComponent('Yes', ModelEditorPopups.button, ModelEditorPopups.hoverButton, function(x, y, button){
+						editor.save();
+						editor.exit();
+					}), 0.1, 0.2, 0.3, 0.4);
+					this.addComponent(new Gui.TextComponent('No', ModelEditorPopups.button, ModelEditorPopups.hoverButton, function(x, y, button){
+						editor.exit();
+					}), 0.35, 0.2, 0.55, 0.4);
+					this.addComponent(new Gui.TextComponent('Cancel', ModelEditorPopups.button, ModelEditorPopups.hoverButton, function(x, y, button){
+						editor.setPopup(null);
+					}), 0.6, 0.2, 0.85, 0.4);
+				});
+				editor.setPopup(popup);
+			} else {
+				editor.exit();
+			}
+		}), 0.05);
+	});
+	return toolbar;
+};
